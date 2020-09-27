042930c360124f4d378aec70a288a5f5c60533e5
xiaoqingshu2019 <1826005545@qq.com>
2020-09-27 12:51:24 +0800

    machine learning

---
+++ jiqixuexi.py
@@ -0,0 +1,5 @@
+from keras import models
+from keras import layers
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu')
